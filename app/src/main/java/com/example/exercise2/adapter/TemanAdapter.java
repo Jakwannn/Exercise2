eddea044fea216124988db72c332fbf38cb32e82
ndzakwan <n.dzakwan9@gmail.com>
2021-05-02 23:39:54 +0700

    Naufal Dzakwan

---
+++ app/src/main/java/com/example/exercise2/adapter/TemanAdapter.java
@@ -0,0 +1,112 @@
+package com.example.exercise2.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.exercise2.EditTeman;
+import com.example.exercise2.MainActivity;
+import com.example.exercise2.R;
+
+import com.example.exercise2.database.DBController;
+import com.example.exercise2.database.Teman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+    private ArrayList<Teman> listData;
+    private Context c;
+
+    public TemanAdapter(ArrayList<Teman> listData) {
+        this.listData = listData;
+    }
+
+    public TemanAdapter(Context c, ArrayList<Teman> listData){
+
+        this.listData = listData;
+    }
+    @Override
+    public TemanAdapter.TemanViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        c = parent.getContext();
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        return new TemanViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TemanViewHolder holder, int position) {
+        String nm,tlp,id;
+
+        id = listData.get(position).getId();
+        nm = listData.get(position).getNama();
+        tlp = listData.get(position).getTelpon();
+        DBController db = new DBController(c);
+
+        holder.namaTxt.setText(nm);
+        holder.namaTxt.setTextSize(20);
+        holder.telponTxt.setText(tlp);
+
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(c, holder.cardku);
+                popupMenu.inflate(R.menu.popup_menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.mnedit:
+                                Intent i = new Intent(c, EditTeman.class);
+                                i.putExtra("id",id);
+                                i.putExtra("nama",nm);
+                                i.putExtra("telpon",tlp);
+                                c.startActivity(i);
+                                break;
+                            case R.id.mndelete:
+                                HashMap<String,String > values = new HashMap<>();
+                                values.put("id",id);
+                                db.deleteData(values);
+                                Intent intent = new Intent(c, MainActivity.class);
+                                c.startActivity(intent);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listData != null)?listData.size() : 0;
+    }
+
+
+
+
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardku;
+        private TextView namaTxt, telponTxt;
+
+        public TemanViewHolder(View view) {
+            super(view);
+            cardku = (CardView) view.findViewById(R.id.kartu);
+            namaTxt = (TextView) view.findViewById(R.id.textNama);
+            telponTxt = (TextView) view.findViewById(R.id.textTelpon);
+
+        }
+    }
+}
